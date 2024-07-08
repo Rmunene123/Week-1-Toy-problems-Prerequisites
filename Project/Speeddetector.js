52b9090b7a3b0f24efa71dee4eb04111b5318e34
Rmunene123 <rosewanmun@gmail.com>
2024-07-08 21:32:02 +0300

    added more comments

--- Project/Speeddetector.js
+++ Project/Speeddetector.js
@@ -1,12 +1,17 @@
+//Define a function called detectSpeed that takes a parameter speed
 function detectSpeed(speed){
+    //check if speed is 70km/h or less
     if(speed <= 70 ){
         console.log("Ok");
         
     }
+    //if speed is greater than 70km/h
     else if(speed > 70){
-        let demeritPoints = 0;
-        let excess = speed - 70;
+        let demeritPoints = 0; //Initialize demeritPoints variable to 0
+        let excess = speed - 70; //Calculate the speed over the limit (excess)
         demeritPoints = Math.floor(excess/5);
+        
+        //Check if demeritPoints exceed 12 for license suspension
         if(demeritPoints > 12)
             {
                 console.log("License suspended");
@@ -15,13 +20,14 @@ function detectSpeed(speed){
             }
        
     }else{
-        console.log("invalid input");
+        console.log("invalid input"); //print invalid input
         
     }
-        return;
+        return; //exit the function
     
 
 }
+//example uses of the function:
 detectSpeed(91);
 detectSpeed(150);
 detectSpeed(70);
